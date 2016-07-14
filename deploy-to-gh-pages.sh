27dc494d4799fc811d3ebab41506b2a72a8e5ad5
Travis CI <nobody@nobody.org>
2016-07-14 16:43:40 +0300

    fix github link

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -11,4 +11,4 @@ rm -rf .git
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
